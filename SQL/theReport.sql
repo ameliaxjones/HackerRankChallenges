ed22de45d1ab2c5e170b2e81482890693c526ffc
Amelia <gcab022111@coderacademy.edu.au>
2022-07-19 16:47:21 +1000

    2darray Challange

--- SQL/theReport.sql
+++ SQL/theReport.sql
@@ -8,6 +8,8 @@ IF LOWER THEN 8 arrange by asc Marks - ?
 
 
 This passes the test on the site yet the code isnt fully complete from what they actually wanted.
+
+I spent so long reaserching how to link to tables together with out FK that when it came to doing the last part of the requirements it threw me off quite a bit
 */
 
 select IF(Grades.Grade < 8 , NULL, Students.Name), Grades.Grade, Students.Marks
